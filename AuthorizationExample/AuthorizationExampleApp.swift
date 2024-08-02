06bc30baa6a7d08430270b38dd244b785cdc7c16
Sherlock <sherlockh989@gmail.com>
2024-08-02 21:52:01 +0300

    Initial Commit

---
+++ AuthorizationExample/AuthorizationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationExampleApp.swift
+//  AuthorizationExample
+//
+//  Created by Андрій Коробчук on 02.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
